c7ac9a1c38c2e957e9741afcc0dbc87130789a4c
Jaimi Litus <Jaimilit@gmail.com>
2025-03-05 12:31:34 +0000

    added Footer & MainContent to their own pages

---
+++ MainContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function MainContent() {
+  return (
+    <div className="main-content"> 
+      <h1>This is the best CrossFit Community</h1>
+            <h3>Join our Box</h3>
+            <ol>
+              <li>Prices</li>
+              <li>Membership</li>
+              <li>Personal Trainers</li>
+              <li>Meet the team</li>
+            </ol>
+    </div>
+  );
+}
--- index.jsx
+++ index.jsx
@@ -1,33 +1,11 @@
 
 import React from "react";
 import Header from "./Header";
-//import Header from "./components/Header"
+import Footer from "./Footer"; 
+import MainContent from "./MainContent";
 import ReactDOM from "react-dom/client";
 
 
-
-function MainContent() {
-  return (
-    <div className="main-content"> 
-      <h1>This is the best CrossFit Community</h1>
-            <h3>Join our Box</h3>
-            <ol>
-              <li>Prices</li>
-              <li>Membership</li>
-              <li>Personal Trainers</li>
-              <li>Meet the team</li>
-            </ol>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2025 CrossFit Community. All rights reserved.</p>
-    </footer>
-  );
-}
 
 
 function Page() { 
